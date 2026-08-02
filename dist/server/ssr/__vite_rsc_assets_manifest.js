0c29c6413cc9b6185d3b4e2534d9aa0b2eea32ee
luckyblur <nxys86@gmail.com>
2026-08-02 14:52:48 -0300

    upload build files

---
+++ dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,49 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-Cg4sijSO.js\")",
+  "clientReferenceDeps": {
+    "6efdf509a785": {
+      "js": [
+        "/assets/page--qvCHuXr.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-DkySH6nJ.js",
+        "/assets/index-Cg4sijSO.js"
+      ],
+      "css": [
+        "/assets/page-BKnyWaLe.css"
+      ]
+    },
+    "593f344dc510": {
+      "js": [
+        "/assets/index-Cg4sijSO.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-DkySH6nJ.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/assets/layout-segment-context-C4reTKIC.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/index-Cg4sijSO.js",
+        "/assets/framework-DkySH6nJ.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/assets/index-Cg4sijSO.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-DkySH6nJ.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-Cfgc8Eti.css"
+      ]
+    }
+  }
+}
